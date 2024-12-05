44cc1e2798918a664f406d8122f0e749da3d1b99
Gajjala Trishna <147131793+trishna2105@users.noreply.github.com>
2024-12-05 10:29:44 +0530

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,22 @@
+class GFG {
+  
+    public static void main(String[] args) {
+      
+        String s = "Program";
+        char[] ch = s.toCharArray();
+        boolean[] b = new boolean[s.length()];
+
+        System.out.println("Character Occurrences:");
+        for (int i = 0; i < ch.length; i++) {
+            if (b[i]) continue;     // Skip already counted characters
+            int c = 1;
+            for (int j = i + 1; j < ch.length; j++) {
+                if (ch[i] == ch[j]) {
+                    c++;
+                    b[j] = true;     // Mark character as counted
+                }
+            }
+            System.out.println(ch[i] + " : " + c);
+        }
+    }
+}
